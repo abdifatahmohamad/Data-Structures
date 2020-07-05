4bd9b89694823830f09e6a42e5154b3ce5285676
Abdifatah Mo <abdifatahdev@Abdifatahs-MacBook-Pro.local>
2020-07-05 15:07:04 -0500

    Added reverse_linked_list file

--- LinkedList/insertHead_insertTail.py
+++ LinkedList/insertHead_insertTail.py
@@ -4,49 +4,51 @@ class Node:
         self.data = data
         self.next = None
 
-
 class Linked_list:
     def __init__(self):
         self.head = None
 
-    def insert_head(self, val):
-        new_node = Node(val)
-        new_node.next = self.head
-        self.head = new_node
-
     def print_list(self):
         if self.head is None:
-            print("list is empty!")
+            print('The list is empty')
             return
+
         curr_node = self.head
-        while curr_node:
-            print(curr_node.data, '-> ', end='')
+        while curr_node is not None:
+            print(curr_node.data, end=" -> ")  # The pointer goes right for insertion at the tail
+            # print(curr_node.data, end=" <- ") # The pointer goes left for insertion at the head
             curr_node = curr_node.next
         print()
 
-    def insert_tail(self, val):
-        new_node = Node(val)
+    def insert_head(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_tail(self, data):
+        new_node = Node(data)
         if self.head is None:
             self.head = new_node
             return
-        cur_node = self.head
-        while cur_node.next:
-            cur_node = cur_node.next
-        cur_node.next = new_node
 
+        last_node = self.head
+        while last_node.next is not None:
+            last_node = last_node.next
+        last_node.next = new_node
 
+llist = Linked_list()
 
+# Intersion at the front:
+llist.insert_head(1)
+llist.insert_head(2)
+llist.insert_head(3)
+llist.insert_head(4)
 
+# Insertion at the end:
+# llist.insert_tail(1)
+# llist.insert_tail(2)
+# llist.insert_tail(3)
+# llist.insert_tail(6)
 
-node = Linked_list()
-
-# node.insert_head(7)
-# node.insert_head(9)
-# node.insert_head(1)
-node.insert_tail(7)
-node.insert_tail(1)
-node.insert_tail(5)
-node.insert_tail(8)
-node.insert_tail(3)
-node.insert_tail(9)
-node.print_list()
+# To display/print our LikedList
+llist.print_list()
