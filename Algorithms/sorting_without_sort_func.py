92b0661f06d614855e24d1f54b34d53eaac742c6
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-16 13:27:07 -0500

    Added a solution

---
+++ Algorithms/sorting_without_sort_func.py
@@ -0,0 +1,14 @@
+# Sorting a list in python without the sorted function
+
+arr = [5, 2, 4, 1]
+for i in range(len(arr)):
+    for j in range(len(arr) - 1):
+        if arr[j] > arr[j + 1]:
+            temp = arr[j + 1]
+            arr[j + 1] = arr[j]
+            arr[j] = temp
+
+            # Same as above code
+            # arr[j + 1], arr[j] = arr[j], arr[j + 1]
+
+print(arr)
