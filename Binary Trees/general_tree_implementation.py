a820e452ef7d2c7453de81c4bbda6207d0786390
Abdifatah Mohamed <mofaatah@gmail.com>
2022-05-22 00:39:22 -0500

    Added general implementation tree.

--- Binary Trees/general_tree_implementation.py
+++ Binary Trees/general_tree_implementation.py
@@ -1,53 +1,55 @@
-# Create general tree in Python // https://www.youtube.com/watch?v=4r_XR9fUPhQ
-class TreeNode(object):
+# Create general tree in Python/ https://www.youtube.com/watch?v=4r_XR9fUPhQ
+class TreeNode:
     def __init__(self, val):
         self.val = val
-        self.childern = []
+        self.children = []
         self.parent = None
 
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+
     def print_tree(self):
-        #spaces = ' ' * self.get_level() * 3
-        #prefix = spaces + "|__" if self.parent else ""
-        # print(prefix + self.data)
-        # if self.children:
-        #     for child in self.children:
-        #         child.print_tree()
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.val)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
 
-        print(self.val)
-        for child in self.childern:
-            print(child.val)
-
-    def add_child_node(self, child):
+    def add_child(self, child):
         child.parent = self
-        self.childern.append(child)
+        self.children.append(child)
 
 
 def build_product_tree():
     root = TreeNode("Electronics")
 
     laptop = TreeNode("Laptop")
-    laptop.add_child_node(TreeNode("Mac"))
-    laptop.add_child_node(TreeNode("Surface"))
-    laptop.add_child_node(TreeNode("Thinkpad"))
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
 
     cellphone = TreeNode("Cell Phone")
-    cellphone.add_child_node(TreeNode("iPhone"))
-    cellphone.add_child_node(TreeNode("Google Pixel"))
-    cellphone.add_child_node(TreeNode("Vivo"))
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
 
     tv = TreeNode("TV")
-    tv.add_child_node(TreeNode("Samsung"))
-    tv.add_child_node(TreeNode("LG"))
+    tv.add_child(TreeNode("Samsung"))
+    tv.add_child(TreeNode("LG"))
 
-    root.add_child_node(laptop)
-    root.add_child_node(cellphone)
-    root.add_child_node(tv)
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
 
-    # root.print_tree()
-    return root
-
-
-if __name__ == "__main__":
-    root = build_product_tree()
     root.print_tree()
-    pass
+
+
+if __name__ == '__main__':
+    build_product_tree()
