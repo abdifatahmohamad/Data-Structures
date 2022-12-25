c0f4dcd76ca1a4f215cb67acea222fef2f90e304
Abdifatah Mohamed <mofaatah@gmail.com>
2022-12-25 14:15:04 -0600

    Added graph traversals

---
+++ Graphs/breadth_first_search.py
@@ -0,0 +1,52 @@
+from collections import defaultdict, deque
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.adjacency = defaultdict(list)
+
+    def create_graph(self, grid):
+        for src, des in grid:
+            self.adjacency[src].append(des)
+            self.adjacency[des].append(src)
+
+    def print_graph(self) -> None:
+        for node in self.adjacency:
+            print(f'{node}, {self.adjacency[node]}')
+
+    # Print using string representation
+    # def __str__(self):
+    #     return str(self.__dict__)
+
+    def BFS(self, start):
+        visited = {start}
+        queue = deque([start])
+        while queue:
+            node = queue.popleft()
+            print(node, end=" ")
+            for neighbor in self.adjacency[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+
+board = [
+    [0, 1],
+    [1, 2],
+    [2, 0]
+]
+
+grid = [['A', 'B'], ['A', 'D'],
+        ['B', 'F'], ['B', 'G'],
+        ['C', 'F'], ['C', 'E'],
+        ['D', 'C'], ['D', 'E']
+        ]
+
+g = Graph()
+g.create_graph(board)
+g.create_graph(grid)
+print("BFS: ", end=" ")
+g.BFS('A')
+print("\n---------------------------")
+print("BFS: ", end=" ")
+g.BFS(0)
---
+++ Graphs/depth_first_search.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.adjacency = defaultdict(list)
+
+    def create_graph(self, grid):
+        for src, des in grid:
+            self.adjacency[src].append(des)
+            self.adjacency[des].append(src)
+
+    def print_graph(self):
+        for node in self.adjacency:
+            print(f'{node} -> {self.adjacency[node]}')
+
+    # DFS recursive
+    def DFS_recursive(self, start):
+        visited = set()
+        res = []
+        self.helper(start, visited, res)
+        # return " ".join(res)
+        print(res)
+
+    def helper(self, node, visited, res) -> None:
+        visited.add(node)
+        res.append(node)
+        for neighbor in self.adjacency[node]:
+            if neighbor not in visited:
+                self.helper(neighbor, visited, res)
+
+    # DFS iterative
+    def DFS_iterative(self, start):
+        visited = set()
+        stack = [start]
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+            print(node, end=" ")
+            for neighbor in self.adjacency[node]:
+                if neighbor not in visited:
+                    stack.append(neighbor)
+
+
+board = [
+    [0, 1],
+    [1, 2],
+    [2, 0]
+]
+
+grid = [['A', 'B'], ['A', 'D'],
+        ['B', 'F'], ['B', 'G'],
+        ['C', 'F'], ['C', 'E'],
+        ['D', 'C'], ['D', 'E']
+        ]
+
+g = Graph()
+g.create_graph(grid)
+g.create_graph(board)
+# g.print_graph()
+print("DFS iterative: ", end=" ")
+g.DFS_iterative('A')
+print()
+g.DFS_iterative(0)
+print("\n-------------------------------")
+print("DFS recursive: ", end=" ")
+g.DFS_recursive('A')
+g.DFS_recursive(0)
